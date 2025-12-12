4b38b92d8c02762e13fde42eb534c61b08d095ec
ZhenyuLi <893652269@qq.com>
2025-12-12 08:34:59 -0500

    fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,3 @@
+#!/bin/bash
 sudo apt-get update
 sudo apt-get install -y git maven ant vim openjdk-8-jdk
-ant ivy-bootstrap
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-echo export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 >> ~/.bashrc
-export PATH="/usr/lib/jvm/java-8-openjdk-amd64/bin:$PATH"
-echo export PATH="/usr/lib/jvm/java-8-openjdk-amd64/bin:$PATH" >> ~/.bashrc
